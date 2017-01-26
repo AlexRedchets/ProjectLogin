9e1c8d9afc9ae8d849f966f28431ea779b1e1986
AlexRedchets <alexey.redchets@gmail.com>
2017-01-26 12:08:40 -0500

    + keep state of Login Activity  + layout-land

--- app/src/main/java/com/alexredchets/projectlogin/LoginActivity.java
+++ app/src/main/java/com/alexredchets/projectlogin/LoginActivity.java
@@ -1,11 +1,15 @@
 package com.alexredchets.projectlogin;
 
 import android.content.Intent;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -13,9 +17,10 @@ public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = LoginActivity.class.getName();
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
@@ -23,12 +28,12 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     @OnClick(R.id.sign_in_button)
-    public void signInClicked(View view){
+    public void signInClicked(View view) {
         Toast.makeText(this, "Sign In Button Clicked", Toast.LENGTH_SHORT).show();
     }
 
     @OnClick(R.id.sign_up_button)
-    public void signUpClicked(View view){
+    public void signUpClicked(View view) {
         Toast.makeText(this, "Sign Up Button Clicked", Toast.LENGTH_SHORT).show();
 
         Intent i = new Intent(this, SignUpActivity.class);
